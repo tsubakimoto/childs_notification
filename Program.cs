e11c88e7e75d72f39045c3f3619c64faaf76a9ac
tsubakimoto <tkmbmbr@gmail.com>
2018-03-10 16:19:41 +0900

    enable ApplicationInsignts log

--- Program.cs
+++ Program.cs
@@ -20,6 +20,7 @@ namespace childs_notification
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseApplicationInsights()
                 .Build();
     }
 }
