aa7e7a79513150303166bba5b4b8f3a8968b9212
tsubakimoto <tkmbmbr@gmail.com>
2018-03-10 15:42:52 +0900

    Initial project

---
+++ Models/AppSettings.cs
@@ -0,0 +1,13 @@
+namespace childs_notification.Models
+{
+    public class AppSettings
+    {
+      public LineSettings LineSettings { get; set; }
+    }
+    public class LineSettings
+    {
+        public string ChannelSecret { get; set; }
+        public string ChannelAccessToken { get; set; }
+        public string StorageConnectionString { get; set; }
+    }
+}
